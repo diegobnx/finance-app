85d4bf1ec85f0df396351ef341da71ddbd4b93f8
Diego Marin <diego.dmnetwork@gmail.com>
2025-04-15 17:36:51 -0300

    changes

--- frontend/src/hooks/useContas.ts
+++ frontend/src/hooks/useContas.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { Conta, ContaCreate } from "../types/conta";
 
-const API_URL = "http://localhost:8000/api/v1/contas";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export function useContas() {
   const [contas, setContas] = useState<Conta[]>([]);
