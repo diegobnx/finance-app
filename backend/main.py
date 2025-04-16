e96c91d90da0ce0632874117cf2af94c511934e2
Diego Marin <diego.dmnetwork@gmail.com>
2025-04-15 21:23:46 -0300

    changes

--- backend/main.py
+++ backend/main.py
@@ -5,16 +5,19 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.errors import ServerErrorMiddleware
 
 from app.api.v1 import contas
 from app.core.db import engine, Base
 
 app = FastAPI(title="Controle Financeiro", version="1.0.0")
 
+app.add_middleware(ServerErrorMiddleware, debug=True)
+
 # CORS (ajustar conforme necessidade do frontend)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://192.168.0.110:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
