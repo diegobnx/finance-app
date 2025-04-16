f0b1ca8abddf3ac56663cbd3825cb43ad7344b07
Diego Marin <diego.dmnetwork@gmail.com>
2025-04-15 23:15:42 -0300

    changes

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,6 +2,7 @@ import os
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongo:27017")
 DATABASE_NAME = os.getenv("DATABASE_NAME", "financeapp")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:postgres@postgres:5432/financeapp")
 
 if not MONGO_URI or not DATABASE_NAME:
     raise ValueError("Variáveis de ambiente MONGO_URI e DATABASE_NAME são obrigatórias.")
