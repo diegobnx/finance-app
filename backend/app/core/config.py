31e56c69aed59aba6895598f746bebf25dd76ad9
Diego Marin <diego.dmnetwork@gmail.com>
2025-04-17 15:29:23 -0300

    alteração de ip

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,5 +3,7 @@ import os
 DATABASE_NAME = os.getenv("DATABASE_NAME", "financeapp")
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:postgres@postgres:5432/financeapp")
 
-if not MONGO_URI or not DATABASE_NAME:
-    raise ValueError("Variáveis de ambiente MONGO_URI e DATABASE_NAME são obrigatórias.")
+if not DATABASE_URL or not DATABASE_NAME:
+    raise ValueError(
+        "Variáveis de ambiente DATABASE_URL e DATABASE_NAME são obrigatórias."
+    )
